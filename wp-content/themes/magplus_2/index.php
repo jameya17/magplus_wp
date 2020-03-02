54043e5f2c53607658dc147d6181e6498f319834
amjadhav <amjadhav@gracenote.com>
2020-03-02 19:03:13 +0530

    Files modified

--- wp-content/themes/magplus_2/index.php
+++ wp-content/themes/magplus_2/index.php
@@ -1,96 +1,135 @@
 <?php get_header(); ?>
 
-<div class="full-width post-thumbnail brand-page-header-container" style="background: url('https://d3qvq3btfltx1c.cloudfront.net/wp-content/uploads/2011/10/20022835/blog-bg-1-1-1.jpg') no-repeat top center; background-size: cover;">
-	
+<div class="container"> 
+        <section id="tutorials" class="l-section sec-pad pad-top0 blog-listing-section">
+            <div class="l-section-wrap">
+                <div class="breadcrumb">
+                    <ul>
+                        <li class="breadcrumb-item"><a href="#">Blog</a></li>
+                    </ul>
+                </div>
+                <div class="g-col offset_default">
+                    <div class="main one-third">
+                        <div class="blog-listing">
+                        	<?php 
+                        		if(have_posts()): while(have_posts()): the_post(); 
+                        			$thumb = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+                        	?>
+                            <div class="card card-with-image item">
+                                <div class="card-body">
+                                    <div class="card-image">
+                                        <img src="<?php echo $thumb[0]; ?>" alt="">  
+                                    </div>
+                                    <div class="card-body-con">
+                                        <span class="publish-date"><?php the_time('F d, Y'); ?></span>
+                                        <h5 class="card-title two-ellipsis"><?php the_title(); ?></h5>
+                                        <p class="card-discp block-ellipsis"><?php echo get_the_excerpt(); ?></p>
+                                        <a href="<?php the_permalink(); ?>" class="text-link">Read More +</a>
+                                    </div>    
+                                </div>
+                            </div>
+                            <?php endwhile; endif; ?>
+                        </div>
 
+                        <ul class="pagination">
+                            <li class="page-item" id="previous-page"><a class="page-link" href="javascript:void(0)"><img src="images/icons/pagination-prev.svg" alt=""></a></li>
+                            <li class="page-item current-page active"><a class="page-link" href="javascript:void(0)">1</a></li>
+                            <li class="page-item current-page"><a class="page-link" href="javascript:void(0)">2</a></li>
+                            <li class="page-item current-page"><a class="page-link" href="javascript:void(0)">3</a></li>
+                            <li class="page-item current-page"><a class="page-link" href="javascript:void(0)">4</a></li>
+                            <li class="page-item current-page"><a class="page-link" href="javascript:void(0)">5</a></li>
+                            <li class="page-item"><a class="page-link" href="javascript:void(0)">...</a></li>
+                            <li class="page-item current-page"><a class="page-link" href="javascript:void(0)">9</a></li>
+                            <li class="page-item" id="next-page"><a class="page-link" href="javascript:void(0)"><img src="images/icons/pagination-next.svg" alt=""></a></li>
+                        </ul>
+                    </div>
 
-<div class="page-header" style="margin-top:0px; margin-bottom: 0;">
-	<h1 class="h2">mag+ BLOG</h1>
-</div>
-	
-		<div class="blog-brand-page-header">
-		<h2 class="h4">Updates and news about the app industry<br> and our app-creation tools. </h2>
+                    <div class="sidebar post-sidebar one-cal">
+                        <div class="sidebar-section-listing">
+                            <h3 class="sec-title">Categories</h3>
+                            <ul class="sidebar-listing">
+                            	<?php 
+                                    $categories = get_categories(); 
+                                    foreach($categories as $categoryKey => $category){
+                                    $link = get_category_link($category->cat_ID);
 
-	</div>
-	
-</div>
-<div id="content" class="content group wrapper"> 
-	
-	<div class="main main-blog main-one-sidebar" role="main">
-		
-		<?php 
-		if(is_home()){
+                                ?>
+                                    <li><a href="<?php echo $link; ?>"><?php echo $category->name ?><span class="count"> (<?php echo $category->count ?>)</span></a></li>
+                                <?php } ?>
+                            </ul>
+                        </div>                                             
+                        <div class="sidebar-section-listing">
+                            <h3 class="sec-title">Latest Post</h3>
+                            <?php
+                                $args = array(
+                                    'post_type' => 'post',
+                                    'post_status' => 'publish',
+                                    'orderby' => 'publish_date',
+                                    'order' => 'DESC',
+                                    'posts_per_page' => 4,
+                                );
+                                $the_query = new WP_Query( $args );
 
-			echo '<h1>&nbsp;</h1>';
-		}
-		?>
+                            ?>
+                            <ul class="sidebar-listing">
+                                <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+                                <li><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></li>
+                                <?php endwhile; ?>
+                            </ul>
+                        </div>                                             
+                        <div class="sidebar-section-listing">
+                            <h3 class="sec-title">Tags</h3>
+                            <ul class="sidebar-listing tags-listing">
+                                <?php 
+                                    $tags = get_tags();
+                                    foreach($tags as $tagKey => $tag){
+                                        if($tag->count > 4){
+                                ?>
+                                            <li class="tag-link"><a href="<?php echo get_term_link($tag->term_id); ?>" title="<?php echo $tag->name; ?>"><?php echo $tag->name; ?></a></li>
+                                <?php
+                                        }
+                                    }
+                                ?>
+                            </ul>
+                        </div>  
+                        <div class="sidebar-section-listing">
+                            <h3 class="sec-title">Share</h3>
+                            <ul class="sidebar-listing follow-us-listing">
+                                <li><a href="" title="Slack"><img src="<?php bloginfo('template_directory'); ?>/images/icons/grey-slack.svg" alt="Slack"></a></li>
+                                <li><a href="" title="Twitter"><img src="<?php bloginfo('template_directory'); ?>/images/icons/grey-twitter.svg" alt="Twitter"></a></li>
+                                <li><a href="" title="facebook"><img src="<?php bloginfo('template_directory'); ?>/images/icons/grey-facebook.svg" alt="facebook"></a></li>
+                                <li><a href="" title="G+"><img src="<?php bloginfo('template_directory'); ?>/images/icons/grey-gplus.svg" alt="G+"></a></li>
+                                <li><a href="" title="Instagram"><img src="<?php bloginfo('template_directory'); ?>/images/icons/grey-instagram.svg" alt="Instagram"></a></li> 
+                            </ul>
+                        </div> 
+                        <div class="sidebar-section-listing">
+                            <div class="get-update-ces">
+                                <h3 class="sec-title">Get Updates</h3>            
+                                <div class="subscription-sec">
+                                    <div class="subscription-form">
+                                        <div class="inline-form error-block">
+                                            <input type="email" id="email" placeholder="Enter your email" name="email">
+                                            <button type="button" id="btnClick" alt="newsletter-submit" class="btn"></button>
+                                            <span class="error"></span>
+                                        </div>    
+                                        <div class="tmc-block ">
+                                            <label class="checkbox-container" for="">
+                                                <input type="checkbox">
+                                                <span class="checkmark"></span>
+                                                I agree to receive timely updates with the latest blog posts
+                                            </label>
+                                        </div>    
+                                    </div>
+                                </div>    
 
-		<?php if(have_posts()): while(have_posts()): the_post(); ?>
-		
-		 <h2 class="entry-title blog-title">
-						<a href="<?php the_permalink(); ?>" title="<?php echo the_title_attribute(); ?>"><?php the_title(); ?></a>
-					</h2>
-			<div class="post-meta">
-						<span style="display:none;" class="vcard author author_name"><span class="fn"><?php the_author(); ?></span></span>
-						<?php _e(''); ?> <time class="dt-published entry-updated" datetime="<? the_date('y-m-d'); ?>"><?php the_time('F d, Y'); ?></time>, <?php _e(''); ?> 
-						<?php if ( count( get_the_category() ) ) : ?>
-						<span class="cat-links">
-							<?php printf( __( '<span class="%1$s"></span> %2$s', 'magplus_theme' ), 'entry-utility-prep entry-utility-prep-cat-links', get_the_category_list( ', ' ) ); ?>
-						</span><span><?php echo the_tags();?></span>
-					<?php endif; ?>
-						
-						
-				</div>
-					
-					
-			<article id="post-<?php the_ID(); ?>" <?php post_class('group'); ?>>
-			
-			 
-					
-				<header class="entry-header">
-				<?php if ( has_post_thumbnail() ):?>
-				<div class="featured-image-wrap"><?php the_post_thumbnail( 'large' ); ?></div>
-				<div style="margin-bottom: 40px;"></div>
-				<?php endif; ?>
-				
-					
-				</header><!-- .entry-header -->
-			
-				<div class="entry-content">
-					<?php the_excerpt(); ?>
-					<a href="<?php the_permalink(); ?>"><?php _e('READ MORE', 'magplus_theme'); ?> &raquo;</a>
-					<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'magplus_theme' ), 'after' => '</div>' ) ); ?>
-					<?php edit_post_link( __( 'Edit', 'magplus_theme' ), '<span class="edit-link">', '</span>' ); ?>
-				</div><!-- .entry-content -->
-				
-				<div class="post-footer">
-				
-					
-
-				</div>
-				
-			</article><!-- post_class -->
-		<?php endwhile; endif; ?>
-		
-		
-		<?php /* Display navigation to next/previous pages when applicable */ 
-		
-		if(function_exists('wp_pagenavi')){
-			wp_pagenavi(); 
-		}elseif (  $wp_query->max_num_pages > 0 ){ 
-			echo '<div class="post-navigation">';
-				posts_nav_link(' &#8212; ', __('&laquo; Nyare inl&auml;gg'), __('&Auml;ldre inl&auml;gg &raquo;'));
-			echo '</div>';
-		} ?>
-			</div>
-
-	<?php get_sidebar('blog'); ?>
-</div>
-
-
-
-
-
+                                <h6>Contact: <a href="tel:+91 80876 00106" title="tel:+91 80876 00106">+91 80876 00106</a></h6>
+                            </div>
+                        </div>                                             
+                    </div>
+                </div>            
+            </div>   
+        </section>        
+    </div> 
 
 
 <?php get_footer(); ?>
--- wp-content/themes/magplus_2/single.php
+++ wp-content/themes/magplus_2/single.php
@@ -1,82 +1,171 @@
 <?php get_header(); ?>
+<?php 
+	if(have_posts()): while(have_posts()): the_post(); 
+		$thumb = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+	endwhile; endif;
+?>
+<div class="container casestudiesPg"> 
+        <section id="tutorials" class="l-section sec-pad pad-top0">
+            <div class="l-section-wrap">
+                <div class="breadcrumb">
+                    <ul>
+                        <li class="breadcrumb-item"><a href="#">Magpplus</a></li>
+                        <li class="breadcrumb-item"><a href="#">Support</a></li>
+                        <li class="breadcrumb-item"><a href="#">Case Studies</a></li>
+                        <li class="breadcrumb-item"><a href="">Maserati</a></li>
+                    </ul>
+                </div>
+                <div class="g-col offset_default">
+                    <div class="main one-third">
+                        <article class="type-post">
+                            <header class="entry-header">
+                                <div class="post-meta h-entry"> 
+                                	<?php 
+                                		$categoryObj = get_the_category(); 
+                                		foreach($categoryObj as $categoryKey => $category){
+                                	?>
+                                		<a href="" class="category-name"><?php echo $category->name; ?></a>
+                                	<?php
+                                		}
+                                	?>
+                                    <span class="publish-details">
+                                        <time class="dt-published" datetime="19-09-12"><?php the_time('F d, Y'); ?></time>, by <span class="author vcard h-card p-author"><?php the_author_meta( 'user_nicename' , $author_id ); ?>, </span> 
+                                    </span>
+                                </div> 
+                                <h1 class="entry-title"><?php the_title(); ?></h1>  
+                                <p><?php the_excerpt(); ?></p>
+                            </header>
 
-<div id="content" class="content group wrapper"> 
-	
-	<div class="main main-blog main-one-sidebar" role="main">
-	
-		<?php if(have_posts()): while(have_posts()): the_post(); ?>
-			<article id="post-<?php the_ID(); ?>" <?php post_class('group'); ?>>
-				<header class="entry-header">
-					<h1 class="entry-title">
-						<?php the_title(); ?>
-					</h1>
-					<span style="display:none;" class="author_name"><?php the_author(); ?></span>
-					<span style="display:none;" class="vcard author author_name"><span class="fn"><?php the_author(); ?></span></span>
-					<div class="post-meta h-entry">
-						<?php _e('Posted on'); ?> <time class="dt-published" datetime="<? the_date('y-m-d'); ?>"><?=  get_the_date(); ?></time>, <?php _e('by'); ?> 
-						<span class="author vcard h-card p-author">
-							<?php echo get_the_author(); ?>,
-						</span>
-						<p></p>
-						<span><?php echo the_tags();?></span>
-						<span class="tag-text">
-							<?php 
-							custom_tag_text_position( array(
-								'id' => $post->ID,
-								'prefix' => ' | ',
-								'pos' => 1
-							));
-							?>
-						</span>
-					</div>
-					
+                            <div class="entry-content">
+                                <img src="<?php echo $thumb[0]; ?>" class="blog-img" alt="" />
+                                <?php the_content(); ?>
+                            </div>
+                        </article>
+                    </div>
 
-					
-				</header><!-- .entry-header -->
-               
+                    <div class="sidebar post-sidebar one-cal">
+                        <div class="sidebar-section-listing">
+                            <h3 class="sec-title">Categories</h3>
+                            <ul class="sidebar-listing">
+                                <?php 
+                                    $categories = get_categories(); 
+                                    foreach($categories as $categoryKey => $category){
+                                    $link = get_category_link($category->cat_ID);
 
-				<div class="entry-content">
-					<?php the_content(); ?>
-					
-					
-					<?php
-					custom_tag_text_position( array(
-						'id' => $post->ID,
-						'prefix' => '<p>',
-						'suffix' => '</p>',
-						'pos' => 2
-					));
-					?>
-					
-					
-					
-					<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'magplus_theme' ), 'after' => '</div>' ) ); ?>
-					<?php edit_post_link( __( 'Edit', 'magplus_theme' ), '<span class="edit-link">', '</span>' ); ?>
-				</div><!-- .entry-content -->
-				
-				<?php ps_insert_social_buttons( array('icons' => false) ); ?>
-				
-				
-				<div class="subscribe-link">
-					<a href="/feed"><img src="<?php echo get_template_directory_uri(); ?>/images/rss.png" alt="RSS"> Subscribe to our blog</a>
-				</div>
-									
-			</article><!-- post_class -->
-		<?php endwhile; endif; ?>
-		
-		<?php// comments_template( '', true ); ?>
-		
-		<div id="pingbacks">
-			<?php // DisplayPingTrackbacks(); ?>
-		</div>
-	</div>
+                                ?>
+                                    <li><a href="<?php echo $link; ?>"><?php echo $category->name ?><span class="count"> (<?php echo $category->count ?>)</span></a></li>
+                                <?php } ?>
+                            </ul>
+                        </div>                                             
+                        <div class="sidebar-section-listing">
+                            <h3 class="sec-title">Latest Post</h3>
+                            <?php
+                                $args = array(
+                                    'post_type' => 'post',
+                                    'post_status' => 'publish',
+                                    'orderby' => 'publish_date',
+                                    'order' => 'DESC',
+                                    'posts_per_page' => 4,
+                                );
+                                $the_query = new WP_Query( $args );
 
-	<?php //get_sidebar('right'); ?>
-	<?php get_sidebar('blog'); ?>
-</div>
-</div>
+                            ?>
+                            <ul class="sidebar-listing">
+                                <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+                                <li><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></li>
+                                <?php endwhile; ?>
+                            </ul>
+                        </div>                                             
+                        <div class="sidebar-section-listing">
+                            <h3 class="sec-title">Tags</h3>
+                            <ul class="sidebar-listing tags-listing">
+                                <?php 
+                                    $tags = get_tags();
+                                    foreach($tags as $tagKey => $tag){
+                                        if($tag->count > 4){
+                                ?>
+                                            <li class="tag-link"><a href="<?php echo get_term_link($tag->term_id); ?>" title="<?php echo $tag->name; ?>"><?php echo $tag->name; ?></a></li>
+                                <?php
+                                        }
+                                    }
+                                ?>
+                            </ul>
+                        </div>  
+                        <div class="sidebar-section-listing">
+                            <h3 class="sec-title">Share</h3>
+                            <ul class="sidebar-listing follow-us-listing">
+                                <li><a href="" title="Slack"><img src="<?php bloginfo('template_directory'); ?>/images/icons/grey-slack.svg" alt="Slack"></a></li>
+                                <li><a href="" title="Twitter"><img src="<?php bloginfo('template_directory'); ?>/images/icons/grey-twitter.svg" alt="Twitter"></a></li>
+                                <li><a href="" title="facebook"><img src="<?php bloginfo('template_directory'); ?>/images/icons/grey-facebook.svg" alt="facebook"></a></li>
+                                <li><a href="" title="G+"><img src="<?php bloginfo('template_directory'); ?>/images/icons/grey-gplus.svg" alt="G+"></a></li>
+                                <li><a href="" title="Instagram"><img src="<?php bloginfo('template_directory'); ?>/images/icons/grey-instagram.svg" alt="Instagram"></a></li> 
+                            </ul>
+                        </div> 
+                        <div class="sidebar-section-listing">
+                            <div class="get-update-ces">
+                                <h3 class="sec-title">Get Updates</h3>            
+                                <div class="subscription-sec">
+                                    <div class="subscription-form">
+                                        <div class="inline-form error-block">
+                                            <input type="email" id="email" placeholder="Enter your email" name="email">
+                                            <button type="button" id="btnClick" alt="newsletter-submit" class="btn"></button>
+                                            <span class="error"></span>
+                                        </div>    
+                                        <div class="tmc-block ">
+                                            <label class="checkbox-container" for="">
+                                                <input type="checkbox">
+                                                <span class="checkmark"></span>
+                                                I agree to receive timely updates with the latest blog posts
+                                            </label>
+                                        </div>    
+                                    </div>
+                                </div>    
 
+                                <h6>Contact: <a href="tel:+91 80876 00106" title="tel:+91 80876 00106">+91 80876 00106</a></h6>
+                            </div>
+                        </div>                                             
+                    </div>
+                </div>            
+            </div>   
+        </section>
 
-
-
+        <section class="l-section fiveCol-slider sec-pad pad-top0">
+            <div class="l-section-wrap">
+                <div class="sec-holder">
+                    <h3>More Blogs</h3>     
+                    <div class="slider-container">
+                        <div class="nonloop owl-carousel col-carousel out-of-nav five-col-carousel"> 
+                        	<?php
+                                $args = array(
+                                    'post_type' => 'post',
+                                    'post_status' => 'publish',
+                                    'orderby' => 'publish_date',
+                                    'order' => 'DESC',
+                                    'posts_per_page' => 7
+                                );
+                                $the_query = new WP_Query( $args );
+                                while ( $the_query->have_posts() ) : $the_query->the_post();
+                                $thumb = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+                            ?>
+                            	<div class="card card-with-image item">
+	                                <div class="card-body">
+	                                    <div class="card-image">
+	                                        <img src="<?php echo $thumb[0]; ?>" alt="" />  
+	                                    </div>
+	                                    <div class="card-body-con">
+	                                        <span class="publish-date"><?php the_time('F d, Y'); ?> </span>
+	                                        <h5 class="card-title two-ellipsis"><?php the_title(); ?></h5>
+	                                        <p class="card-discp block-ellipsis"><?php echo get_the_excerpt(); ?></p>
+	                                        <a href="<?php the_permalink(); ?>" class="text-link">Read More +</a>
+	                                    </div>    
+	                                </div>
+	                            </div>
+                            <?php endwhile; ?>
+                        </div> 
+                        <div class="slider-counter">1/5</div>
+                    </div>                                                              
+                </div>    
+            </div>    
+        </section>
+    </div>
 <?php get_footer(); ?>
